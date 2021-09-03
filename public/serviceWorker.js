b8cf6505675343e08eebfefc420f193558a99e0c
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-04 00:04:12 +0530

    Minor changes

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -18,9 +18,7 @@ self.addEventListener('fetch', (event) => {
         caches.match(event.request)
             .then(() => {
                 return fetch(event.request)
-                    .catch(() => {
-                        caches.match('offline.html')
-                    })
+                    .catch(() => caches.match('offline.html'))
             })
     )
 });
@@ -32,10 +30,8 @@ self.addEventListener('activate', (event) => {
         caches.keys()
         .then((cacheNames) => {
             Promise.all(cacheNames.map((cacheName) => {
-                if(!cacheWhiteList.includes(cacheName)) {
+                if(!cacheWhiteList.includes(cacheName))
                     return caches.delete(cacheName);
-                }
-                return ''
             }))
         })
     )
