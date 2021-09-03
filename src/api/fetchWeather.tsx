58cae5e194c7c7234ddda24fc33c41177e858bba
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-03 18:09:39 +0530

    Created request function for API

---
+++ src/api/fetchWeather.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const ENDPOINT = 'https://api.https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '43b9a28bd0b2288028c4ecdda7e21624'
+
+export const fetchWeather = async (loc: any) => {
+    const response = await axios.get(ENDPOINT, {
+        params: {
+            q: loc,
+            units: 'metric',
+            APPID: API_KEY,
+
+        }
+    });
+    return response.data;
+}
