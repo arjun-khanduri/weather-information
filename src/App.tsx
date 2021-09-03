4c215282b60f4b608909924b82df4d33d7bb740e
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-03 18:44:27 +0530

    Successfully displaying fetched information

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { fetchWeather } from './api/fetchWeather';
+import './App.css';
 
 const App = () => {
     const [location, setLocation] = useState('');
+    const [weather, setWeather] = useState({ main: { temp: 1000 }, name: '', weather: [{icon:'', description:''}], sys: { country: '' } });
+
     const search = async (e: any) => {
         if (e.key === 'Enter') {
             const data = await fetchWeather(location)
-            console.log(data)
+            setWeather(data);
+            setLocation('');
         }
     }
     return (
@@ -18,6 +22,18 @@ const App = () => {
                 value={location}
                 onChange={(e) => setLocation(e.target.value)}
                 onKeyPress={search} />
+            {weather.main.temp !== 1000 && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
